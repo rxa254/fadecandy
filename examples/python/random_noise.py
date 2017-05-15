2dac663612c7d6c797efefbba7d0a347d89b0f2b
rxa <rana@caltech.edu>
2017-05-15 15:58:33 -0700

    adjusted the sleep time between changes

--- examples/python/random_noise.py
+++ examples/python/random_noise.py
@@ -3,21 +3,36 @@
 # Use the FadeCandy drivers to send random numbers into 64 RGB LEDs
 from __future__ import division
 import numpy as np
-import opc, time
+import opc, time, sys, signal
+import argparse
+
+parser = argparse.ArgumentParser(description='Randomly Flash the LEDs.')
+parser.add_argument('refresh_period', metavar='tau', type=float,
+                    help='LED Refresh Period [ms]')
+args = parser.parse_args()
+
+tau = args.refresh_period
 
 numLEDs = 64
 client = opc.Client('localhost:7890')
 
-g = np.floor(np.random.rand(numLEDs, 3) * 255)
-
 black = [ (0,0,0) ] * numLEDs
 white = [ (255,255,255) ] * numLEDs
 
-sleepTime = 0.02
+# catch the CTRL-C and die gracefully
+def sigint_handler(signum, frame):
+        print('\n')
+        print('CTRL-C Encountered...Shutting down.\n')
+        client.put_pixels(black)
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, sigint_handler)
+
+
 
 while True:
     g = np.floor(np.random.rand(numLEDs, 3) * 255)
-    sleepTime = 0.2 + np.random.random_sample()/2
+    sleepTime = tau + np.random.random_sample()
 
     client.put_pixels(g)
     time.sleep(sleepTime)
